60d839e1c8c8fdc4d649def01eab52d8f0ed3191
Radek Polak <psonek2@seznam.cz>
2013-02-19 21:35:46 +0100

    basic milling path in go

--- millgo/main.go
+++ millgo/main.go
@@ -7,6 +7,7 @@ import (
 "fmt"
 "unsafe"
 //"math/rand"
+"image"
 "image/png"
 )
  
@@ -18,17 +19,22 @@ func draw_point(x int32,y int32,value uint32,screen* sdl.Surface) {
   pp = (*uint32)(pu);
   *pp = value;
 }
- 
+
+func pixSet(x, y int32, img image.Image) bool {
+    col := img.At(int(x), int(y))
+    r, g, b, a := col.RGBA()
+    return r + g + b + a > 32768 * 3 
+}
+
+// Return true if point x,y is inside of cirlcle with center cz,cy and radius r
+func inRange(x, y, cx, cy, r int32) bool {
+    var dx int32 = cx - x
+    var dy int32 = cy - y
+    return dx * dx + dy * dy <= r * r
+}
+
 func main() {
  
-  var screen = sdl.SetVideoMode(1440, 900, 32, sdl.RESIZABLE)
- 
-  if screen == nil {
-    log.Fatal(sdl.GetError())
-  }
-  
-  
-  
   var fname string
   fname = "case1.png"
     file, err := os.Open(fname)
@@ -45,19 +51,85 @@ func main() {
     }
 
     b := pic.Bounds()
+    var width, height int32 = int32(b.Max.X - b.Min.X), int32(b.Max.Y - b.Min.Y)
+    var x, y, xx, yy int32
+    
+    var screen = sdl.SetVideoMode(int(width), int(height), 32, sdl.RESIZABLE)
+ 
+    if screen == nil {
+        log.Fatal(sdl.GetError())
+    }
 
-    for y := b.Min.Y; y < b.Max.Y; y++ {
-        for x := b.Min.X; x < b.Max.X; x++ {
-            col := pic.At(x, y)
-            
-            r, g, b, a := col.RGBA()
-            //fmt.Fprintf(os.Stderr, "x=%d y=%d r=%d g=%d b=%d\n", x, y, r, g, b)
-            if r + g + b + a > 32768 * 3 {
-                draw_point(int32(x),int32(y), 0xffffffff,screen);
+    
+    for y = 0; y < height; y++ {
+        for x = 0; x < width; x++ {
+            if !pixSet(x, y, pic) {
+                draw_point(x,y, 0xff0000,screen);
             }
         }
     }
-                    screen.Flip();
+    
+    var r int32 = 15
+    
+    for y = 0; y < height; y++ {
+
+        screen.Flip();
+
+        for x = 0; x < width; x++ {
+            if !pixSet(x, y, pic) {
+                continue
+            }
+
+            //fmt.Printf("%d, %d\n", x, y) 
+            //draw_point(x,y, 0xff,screen)
+
+            ok := true
+            
+            for xx = x - r; xx <= x + r; xx++ {
+                
+                if xx < 0 || xx >= width {
+                    continue
+                }                
+            
+                for yy = y - r; yy <= y + r; yy++ {
+                    if yy < 0 || yy >= height {
+                        continue
+                    }
+                    
+                    if !inRange(xx, yy, x, y, r) {
+                        continue
+                    }
+                    
+                    if !pixSet(xx, yy, pic) {
+                        ok = false
+                        //draw_point(xx,yy, 0xff00,screen)      // milling material that should stay
+                    }
+                }
+            }
+ 
+            if ok {
+                
+                for xx = x - r; xx <= x + r; xx++ {
+                    
+                    if xx < 0 || xx >= width {
+                        continue
+                    }                
+                
+                    for yy = y - r; yy <= y + r; yy++ {
+                        if yy < 0 || yy >= height {
+                            continue
+                        }                        
+                        if inRange(xx, yy, x, y, r) {
+                            draw_point(xx,yy, 0xffff,screen)
+                        }
+                    }
+                }
+            }            
+        }
+    }
+                
+         
+        screen.Flip();
 
                     for true {
                     }
